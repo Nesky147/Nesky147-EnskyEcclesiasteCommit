91b4f82d5bfd6b5cefe3f990a16735f0363b33e1
Nesky147 <ecclesiastensky@gmail.com>
2025-11-11 00:04:04 -0500

    Add files via upload

--- ecclesiasteen/at03/at03Ex02.java
+++ ecclesiasteen/at03/at03Ex02.java
@@ -3,28 +3,40 @@ package ecclesiasteen.at03;
 import java.util.Scanner;
 
 public class at03Ex02 {
-    static void probleme2() {
+    static void probleme1() {
          Scanner scanner = new Scanner(System.in);
+         
+         //Declaration de la variable
+         String [] smot = new String[5];
 
-         //tableau de réels de 10 chiffres
-        double[] iNombre = new double [10]; 
-        Scanner scanner = new Scanner(System.in);
+         //Contenu du tableau
+         smot[0] = "Jona";
+         smot[1] = "Saad";
+         smot[2] = "David";
+         smot[3] = "Momo";
+         smot[4] = "Joyce";
 
-        //demande a l'utilisateur de saisir 10 nombre réels
-        for(int i = 0; i < iNombre.length; i++){
-            System.out.print("entrez le nombre réel #" + (i + 1) + " : ");
-            iNombre[i] = scanner.nextDouble();
+         //Affichage des mots seulement
+         System.out.println("Mot seulement :");
+
+        //Pour
+        for (String mot : smot) {
+            System.out.println(mot);
         }
-        //affiche le tableau avec les 10 chiffres
-        System.out.println("\n-- Contenu du tableau --");
-        for (int i = 0; i < iNombre.length; i++) {
-            System.out.println(i +" : " + iNombre[i]);
+
+         //Affichage du tableau complet
+         System.out.println("\nContenu du tableau :");
+
+         //Pour
+        for (int i = 0; i < smot.length; i++) {
+            System.out.println(i + " : " + smot[i]);
         }
-        
+
+
         scanner.close();
     }
      public static void main(String[] args) {
-        probleme2();
+        probleme1();
         
      }
 
